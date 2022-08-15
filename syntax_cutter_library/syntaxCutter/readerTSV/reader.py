8b6ac8d2289252420454932b35c50605de15ba62
rabauti <rabauti@gmail.com>
2022-08-15 12:58:12 +0300

    Fixed: sentence global id

--- syntax_cutter_library/syntaxCutter/readerTSV/reader.py
+++ syntax_cutter_library/syntaxCutter/readerTSV/reader.py
@@ -24,15 +24,24 @@ class Reader(reader.Reader):
         self.logInfo('Reading sentences in progress.')
         if mode not in ['graph', 'text']:
             raise Exception("Unknown mode %s", mode)
-        word_id = 0
+        #global line counter
         count = 0
+        #current sentence data
         current_sentence = []
-        #
+        # sentence graph object
         G = sentence.Sentence()
+        # current collection id
         colId = None
+        # prev collection id
         prevCol = 0
+        #global sentence id - collid_sentensespanstart_sentencecountincollection
         global_sent_id = None
+        #sentence counter in collection
+        coll_sentence_counter = 0
+        sentence_span_start = 0
+        #total lines in TSV file for progressbar calculations
         total_lines = self.count_lines()
+
         with open(self.__FILE) as f:
             for line in tqdm(f, total=total_lines, desc='TSV lines'):
                 unsaved = 1
@@ -43,15 +52,21 @@ class Reader(reader.Reader):
                     self.logError(line)
                     raise Exception(f'Wrong columns number line number {count} in TSV {self.__FILE}')
                 data = {}
+                if not prevCol == colId:
+                    coll_sentence_counter = 0
                 prevCol = colId
                 prev_global_sent_id = global_sent_id
+                prev_sentence_span_start = sentence_span_start
                 (colId, data['start'], data['id'], data['form'], data['lemma'], data['upostag'], data['deprel'], data['head'], data['feat']) = row
                 colId = int(colId)
                 for k in ('id', 'start', 'head'):
                     data[k] = int(data[k])
                 #sentence start
                 if data['id'] == 1:
+                    coll_sentence_counter += 1
+                    sentence_span_start = data['start']
                     if len(G.nodes):
+                        global_sent_id = '%i_%i_%i' % (prevCol, prev_sentence_span_start, coll_sentence_counter , )
                         current_sentence_text = ' '.join([s['form'] for s in current_sentence])
                         unsaved = 0
                         if mode == 'graph':
