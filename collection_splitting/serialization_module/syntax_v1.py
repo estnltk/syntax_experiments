d30be1f90721b246c983f9d456644d3efad4ba4f
Kaire Koljal <kaire.koljal@gmail.com>
2023-01-30 17:51:29 +0200

    module fixes

--- collection_splitting/serialization_module/syntax_v1.py
+++ collection_splitting/serialization_module/syntax_v1.py
@@ -20,14 +20,21 @@ def layer_to_dict(layer):
     }
 
     spans = []
-    attributes = [attr for attr in layer.attributes if attr not in {'parent_span', 'children'}]
+    attributes = []
+    for attr in layer.attributes:
+        if attr == "root":
+            if type(annotation[attr])!="estnltk_core.layer.span.Span":
+                attributes.append(attr)
+        if attr not in {'parent_span', 'children'}:
+            attributes.append(attr)
+            
     for span in layer:
         annotation_dict = []
         for annotation in span.annotations:
             for attr in attributes:
                 if attr == "root":
                     annotation_dict.append({"root_id": annotation[attr]['id'] })
-                    annotation_dict.append({attr: None })
+                    annotation_dict.append({attr: annotation[attr] })
                 else:
                     annotation_dict.append({attr: annotation[attr] })
         spans.append({'base_span': span.base_span.raw(),
@@ -38,25 +45,12 @@ def layer_to_dict(layer):
 
 
 def dict_to_layer(layer_dict: dict, text_object=None):
-    layer = Layer(name=layer_dict['name'],
-                  attributes=layer_dict['attributes'],
-                  secondary_attributes=layer_dict.get('secondary_attributes', ()),
-                  text_object=text_object,
-                  parent=layer_dict['parent'],
-                  enveloping=layer_dict['enveloping'],
-                  ambiguous=layer_dict['ambiguous'],
-                  )
-    layer.meta.update(layer_dict['meta'])
+    from estnltk.taggers.standard.syntax.syntax_dependency_retagger import SyntaxDependencyRetagger
+    layer = default_dict_to_layer(layer_dict, text_object)
     
-    for span_dict in layer_dict['spans']:
-        base_span = to_base_span(span_dict['base_span'])
-        for annotation in span_dict['annotations']:
-            if list(annotation.keys())[0]=="root_id":
-                root_id = annotation[list(annotation.keys())[0]]
-            elif list(annotation.keys())[0]=="root":
-                span =  text_object["stanza_syntax"].spans[root_id-1]
-                root_id = None 
-                layer.add_annotation(base_span, **{"root":span})
+    for span in layer:
+        for annotation in span.annotations:
+            annotation["root"] = text_object[layer.enveloping].spans[annotation["root_id"]-1]
                 
     return layer
     
