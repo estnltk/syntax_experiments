3a8b475f56169607a77ee5996b645bddee6bacc3
Kaire Koljal <kaire.koljal@gmail.com>
2023-02-15 14:34:35 +0200

    faster phrase extractor decorator

--- collection_splitting/estnltk_patches/consistency_decorator.py
+++ collection_splitting/estnltk_patches/consistency_decorator.py
@@ -9,64 +9,52 @@ import datetime
 
 class ConsistencyDecorator:
     
-    def __init__(self, model_path, output_layer, syntax_layer):
+    def __init__(self, input_type, model_path, output_layer, syntax_layer, morph_layer):
         self.resources_path = model_path
         if not self.resources_path:
             raise ValueError('Missing resources path for StanzaSyntaxTagger')
-        tagger_input_type = "morph_extended"
-        self.stanza_tagger = StanzaSyntaxTagger(input_type=tagger_input_type, input_morph_layer=tagger_input_type, 
-                                            add_parent_and_children=True, resources_path=self.resources_path)
-                                            
-        self.output_layer =  output_layer                               
-        self.ignore_tagger = StanzaSyntaxTagger2(ignore_layer=self.output_layer, input_type="morph_extended",
-                                             input_morph_layer="morph_extended", add_parent_and_children=True,
+                       
+        self.output_layer =  output_layer       
+        self.syntax_layer = syntax_layer 
+        self.morph_layer = morph_layer
+        
+        self.ignore_tagger = StanzaSyntaxTagger2(ignore_layer=self.output_layer, input_type=input_type,
+                                             input_morph_layer=self.morph_layer , add_parent_and_children=True,
                                              resources_path=self.resources_path)
-                                             
-        self.syntax_layer = syntax_layer                                     
+
     
     
-    
-    def __call__(self, text_object, phrase_spans, annotations):
+    def __call__(self, text_object, phrase_span, annotations):
         """
        Phrase extractor decorator. Adds attributes to the phrase extractor output layer.
         """
         # make phrase_spans to layer (for ignore_tagger, las etc)
-        phrases_layer = make_layer_template(self.output_layer, 
-                                                                    ['entity_type', 'free_entity', 'is_valid', 'root_id', 'root'],
-                                                                    None, self.syntax_layer, "syntax_v1")
-        phrases_layer.add_annotation(phrase_spans, **annotations)
+        phrases_layer = Layer(name=self.output_layer,
+              text_object=text_object,
+              attributes=['root_id', 'root'],
+              parent=None,
+              ambiguous=False,
+              enveloping = self.syntax_layer, 
+              serialisation_module="syntax_v1"
+              )                                                            
+        phrases_layer.add_annotation(phrase_span, **annotations)
         
-        txt = copy.deepcopy(text_object)
+        # temporarily add phrases layer 
+        text_object.add_layer(phrases_layer)
+        ignore_layer = self.ignore_tagger.make_layer(text_object, text_object.layers)  
         
-        # shortened sentence layer
-        txt.tag_layer("morph_extended")
-        txt.add_layer(phrases_layer)
-        self.ignore_tagger.tag(txt)
-        
-        # needed for las score
-        self.stanza_tagger.tag( txt )
-        
-        without_entity_layer = "syntax_without_entity"
-        
-        las_score, uas, la = get_graph_edge_difference(txt.stanza_syntax, txt[without_entity_layer], phrases_layer, False)
+        las_score, uas, la = get_graph_edge_difference(text_object["stanza_syntax"], ignore_layer, phrases_layer, False)
 
-        for span in phrases_layer:
-            attributes = {'entity_type': span["entity_type"], 'free_entity': span["free_entity"],
-                          'is_valid': span['is_valid'],
-                          'syntax_conservation_score': las_score, "UnlabelledAttachmentScore":uas, "LabelAccuracy":la }
+        # remove the temporary phrases layer 
+        text_object.pop_layer(self.output_layer)
 
-            annotations.update(attributes)
+        attributes = {
+            'syntax_conservation_score': las_score, 
+            "UnlabelledAttachmentScore":uas, 
+            "LabelAccuracy":la }
+
+        annotations.update(attributes)
+        
         return annotations
     
-    
 
-def make_layer_template(name, attributes, parent, enveloping, module):
-        """Creates and returns a template of the layer."""
-        layer = Layer(name=name,
-                      text_object=None,
-                      attributes=attributes,
-                      parent=parent, 
-                      ambiguous=False ,
-                      enveloping = enveloping, 
-                      serialisation_module=module)
-        return layer
