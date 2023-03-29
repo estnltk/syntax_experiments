c52933831808c73bc57bbac591a5775c0922f9ff
Birgit <sormusbirgit@gmail.com>
2023-03-29 03:49:26 +0300

    created the timeloctagger, a tutorial for it and added a new functionality to the decorator

--- adverbials/estnltk_patches/time_loc_decorator.py
+++ adverbials/estnltk_patches/time_loc_decorator.py
@@ -22,33 +22,72 @@ class TimeLocDecorator:
                        "maa-ala", "mander", "tuba", "asukoht", "linn"]
         self.time_wn = ["kuu", "aasta", "aastaaeg", "ajavahemik", "ajaühik", "nädalapäev", "aeg", "päev"]
 
+        self.verb_obl_loc = [["õppima", "kool", "sg in"], ["kirjutama", "alla", "kool", "sg in"]]
+
     def __call__(self, text_object, phrase_span, annotations):
         """
         Adds three syntax conservation scores for the shortened sentence.
         Shortened sentence is obtained by removing the phrase in the phrase_span from the text.
         """
+
+        # Extract all of the necessary information from the parameters
         obl_root = annotations['root']
+        obl_root_id = annotations['root_id']
         obl_lemma = obl_root.lemma
         obl_form = obl_root.form[0]
         obl_type = None
 
+        # Check if OBL phrase is in locative case, if not return 'None' type
         if obl_form not in self.loc_form:
             annotations.update({
                 'obl_type': obl_type})
             return annotations
 
+        # If OBL is in locative case and the lemma is a pre-determined time word, return 'TIME' type
         if obl_form in self.loc_form and obl_lemma in self.time_lemmas:
             obl_type = "TIME"
             annotations.update({
                 "obl_type": obl_type})
             return annotations
 
+        # If OBL is in locative case and the lemma is a pre-determined location word, return 'LOC' type
         if obl_form in self.loc_form and obl_lemma in self.loc_lemmas:
             obl_type = "LOC"
             annotations.update({
                 "obl_type": obl_type})
             return annotations
 
+        # Find the verb-OBL-case combination. In some cases it is possible to determine whether the
+        # OBL phrase is a location based on the verb phrase it is in. If the current phrase is like this
+        # return 'LOC' type
+        current_head = text_object.head[obl_root_id - 1]
+        prev_head = obl_root_id
+
+        while current_head != 0:
+            prev_head = current_head
+            current_head = text_object.head[current_head - 1]
+
+        verb_lemma = text_object.stanza_syntax.lemma[prev_head - 1]
+        verb_obl = [verb_lemma]
+
+        verb_comp = []
+        for w in text_object.stanza_syntax:
+            if w.head == prev_head and 'compound' in w.deprel:
+                verb_comp.append(w.lemma)
+
+        verb_obl.extend(verb_comp)
+        verb_obl.extend([obl_lemma, obl_form])
+
+        if verb_obl in self.verb_obl_loc:
+            obl_type = "LOC"
+            annotations.update({
+                "obl_type": obl_type})
+            return annotations
+
+        # There are a lot of different words referring to places and it is impossible to create an
+        # exhaustive list. The next section combats this issue by finding time and location words
+        # based on their hypernyms as these are more broad and thus cover more OBL phrases in a
+        # smaller set of words.
         synsets = self.wn[obl_lemma]
         if len(synsets) == 1:
             hypernym = synsets[0].hypernyms
