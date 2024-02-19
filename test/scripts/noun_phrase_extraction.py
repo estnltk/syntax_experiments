95f5966ca1ea83f60d6d5bdef531c76b242b5890
ALiivas <108401356+ALiivas@users.noreply.github.com>
2024-02-19 18:38:07 +0200

    Updated noun phrase types

--- test/scripts/noun_phrase_extraction.py
+++ test/scripts/noun_phrase_extraction.py
@@ -7,15 +7,28 @@ import pandas as pd
 
 # -- initializing phrase extractors for different noun phrase types
 #
+# decorator for filtering out noun phrases that don't have noun as root
+def partofspeech_filter(text, span, annotation):
+    if annotation['root']['upostag'] != 'S':
+        return None
+    return annotation
+
 # head is obl
 obl_phrase_extractor = PhraseExtractor(deprel="obl", syntax_layer="stanza_syntax",
                                    output_layer="obl_phrases")
 # head is nsubj
 nsubj_phrase_extractor = PhraseExtractor(deprel="nsubj", syntax_layer="stanza_syntax",
                                    output_layer="nsubj_phrases")
+# head is nsubj of copula sentence
+nsubj_cop_phrase_extractor = PhraseExtractor(deprel="nsubj:cop", syntax_layer="stanza_syntax",
+                                   output_layer="nsubj_cop_phrases")
 # head is obj
 obj_phrase_extractor = PhraseExtractor(deprel="obj", syntax_layer="stanza_syntax",
                                    output_layer="obj_phrases")
+# head is xcomp (only if noun)
+xcomp_phrase_extractor = PhraseExtractor(decorator=partofspeech_filter, deprel="xcomp", syntax_layer="stanza_syntax",
+                                        output_layer="xcomp_phrases")
+
 
 # -- initializing StanzaSyntaxtagger()
 stanza_tagger = StanzaSyntaxTagger(input_type='morph_analysis', input_morph_layer='morph_analysis',
@@ -31,8 +44,10 @@ df = pd.DataFrame(columns=['phrase', 'text_id', 'start_end', 'phrase_type', 'has
 def extract_noun_phrases(text_obj):
     obl_phrase_extractor.tag( text_obj )
     nsubj_phrase_extractor.tag( text_obj )
+    nsubj_cop_phrase_extractor.tag( text_obj )
     obj_phrase_extractor.tag( text_obj )
-    return text_obj.obl_phrases, text_obj.nsubj_phrases, text_obj.obj_phrases
+    xcomp_phrase_extractor.tag( text_obj )
+    return text_obj.obl_phrases, text_obj.nsubj_phrases, text_obj.nsubj_cop_phrases, text_obj.obj_phrases, text_obj.xcomp_phrases
 
 # creates EstNLTK Text-objects from noun phrase layers
 def create_Text_objects(text_id, phrase_type, phrase_layer):
@@ -69,8 +84,10 @@ def append_data_to_df(all_phrase_text_objects):
 # appends phrases to DataFrame    
 def create_df(text_id, text_obj):
     text_obj.meta['text_id'] = text_id
-    obl_phrases, nsubj_phrases, obj_phrases = extract_noun_phrases(text_obj)
+    obl_phrases, nsubj_phrases, nsubj_cop_phrases, obj_phrases, xcomp_phrases = extract_noun_phrases(text_obj)
     obl_phrase_texts = create_Text_objects(text_id, 'obl_phrase', obl_phrases)
     nsubj_phrase_texts = create_Text_objects(text_id, 'nsubj_phrase', nsubj_phrases)
+    nsubj_cop_phrase_texts = create_Text_objects(text_id, 'nsubj_cop_phrase', nsubj_cop_phrases)
     obj_phrase_texts = create_Text_objects(text_id, 'obj_phrase', obj_phrases)
-    return append_data_to_df(obl_phrase_texts+nsubj_phrase_texts+obj_phrase_texts)
+    xcomp_phrase_texts = create_Text_objects(text_id, 'xcomp_phrase', xcomp_phrases)
+    return append_data_to_df(obl_phrase_texts+nsubj_phrase_texts+nsubj_cop_phrase_texts+obj_phrase_texts+xcomp_phrase_texts)
