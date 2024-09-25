658e27ea3d25575d275f465152f9f012fa900c6c
rabauti <rabauti@gmail.com>
2024-09-25 15:13:20 +0300

    initial commit

---
+++ v31_verb_transactions/my_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J v31
+#SBATCH --partition=intel
+#SBATCH -t 2-0:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rabauti@gmail.com
+
+# your code goes below
+#module load python/3.8.6
+eval "$(conda shell.bash hook)"
+#conda init bash
+conda activate estnltk_collocations_py38
+srun python ./v31.py
