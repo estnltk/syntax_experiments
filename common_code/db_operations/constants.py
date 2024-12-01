8562c9c14e8069d113285a48092bf23367b4e5e6
rabauti <rabauti@gmail.com>
2024-12-01 21:50:25 +0200

    initial commit

---
+++ common_code/db_operations/constants.py
@@ -0,0 +1,10 @@
+"""
+This file contains constants used across SQL-related functionality,
+such as escape characters and SQL-specific keywords.
+"""
+
+# Escape character for reserved words or identifiers in SQL
+SQL_ESCAPE_CHAR = '"'
+
+SQL_ESCAPE_CHARS_START = '`"]'
+SQL_ESCAPE_CHARS_END = '`"]'
